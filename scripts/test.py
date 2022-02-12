7aed6f893ed9ab51f7d05e8127a61355d5381e4a
skunz42 <skunz598@gmail.com>
2022-02-11 19:35:48 -0500

    finished geocode script

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,58 @@
 import requests
+import csv
+import re
 
-base_url = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates"
-params = {"f":"pjson", "SingleLine": "Abilene, TX"}
+def write_to_csv(cities, csv_name):
+    with open(csv_name, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for c in cities:
+            writer.writerow([c['city'], c['id'], c['population'], c['lat'], c['lng']])
 
-print(requests.get(base_url, params=params).json())
+# geocoding via esri rest api
+def geocode_addresses(cities):
+    base_url = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates"
+    params = {"f":"pjson", "SingleLine": "Abilene, TX"}
+
+    for c in cities:
+        params["SingleLine"] = c['city']
+        resp = requests.get(base_url, params=params).json()
+        print(resp['candidates'][0]['address'])
+        c['lat'] = resp['candidates'][0]['location']['y']
+        c['lng'] = resp['candidates'][0]['location']['x']
+
+# needed for proper formatting
+def clean_cities(cities):
+    for c in cities:
+        # get elements of rough string
+        split_name = re.split("-|, | Metro Area", c['city'])
+        city = split_name[0]
+        state = ""
+        for s in split_name:
+            # state identifier
+            if len(s) == 2:
+                state = s
+                break
+        c['city'] = city + ", " + state
+
+# Return w/ Name, ID, Population, and blank Lat/Lng
+def city_factory(row):
+    return {"city": row[0], "id": row[2], 'population': row[3], 'lat': 0.0, 'lng': 0.0}
+
+# get metros from csv
+def get_cities_list(csv_name):
+    # read cities
+    cities = []
+    with open(csv_name, newline='') as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        next(reader, None)
+        for row in reader:
+            cities.append(city_factory(row))
+    return cities
+
+def main():
+    cities = get_cities_list('urbanareas.csv')
+    clean_cities(cities)
+    geocode_addresses(cities)
+    write_to_csv(cities, 'cleancities.csv')
+
+main()
